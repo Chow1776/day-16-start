09c275cb4c4b86aa59785acdd4fbf95106f9ff5e
Chow1776 <casey.howlett@pm.me>
2026-03-01 22:39:24 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import another_module
+
+print(another_module.another_variable)
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(300)
+timmy.speed(1)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+my_screen.exitonclick()
